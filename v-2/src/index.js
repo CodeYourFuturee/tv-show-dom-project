495b319d15cb086f7fd910c26ffcdfd64529b4fd
rebwar <rebwar1.azizi@gmail.com>
2022-11-14 05:56:12 +0000

    for display img install file-loader & config webpack

--- v-2/src/index.js
+++ v-2/src/index.js
@@ -1,8 +1,18 @@
 import "./assets/css/style.css";
 import "./assets/scss/style.scss";
+import logo from "./assets/images/logo.png";
 
 console.log("just for config webpack");
 // const h1 = document.querySelector(".red");
 const h1 = document.getElementsByTagName("h1");
+const divImg = document.createElement("div");
+const img = document.createElement("img");
+const body = document.getElementsByTagName("body")[0];
+
+img.setAttribute("src", logo);
+img.classList.add("img");
+divImg.appendChild(img);
+body.appendChild(divImg);
+
 console.log(h1[0]);
 h1[0].classList.add("red");
--- v-2/webpack.config.js
+++ v-2/webpack.config.js
@@ -9,12 +9,20 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/i, // css file extension regex test for css files to be processed by css-loader and style-loader
         use: ["style-loader", "css-loader"],
       },
       {
         test: /\.s[ac]ss$/i, // sass or scss
         use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i, // png, jpg, jpeg, gif
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
       },
     ],
   },

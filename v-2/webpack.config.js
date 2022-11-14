4399e82bd79d5dc33a7b5160918eda090db670e3
rebwar <rebwar1.azizi@gmail.com>
2022-11-14 09:26:52 +0000

    for make a folder for img inside src

--- v-2/webpack.config.js
+++ v-2/webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
             loader: "file-loader",
             options: {
               name: "[name].[ext]",
+              outputPath: "images",
             },
           },
         ],

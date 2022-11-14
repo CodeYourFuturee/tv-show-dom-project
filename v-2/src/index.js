d5826a027dfd8d1dcba0a62dc659625d9aab40ad
rebwar <rebwar1.azizi@gmail.com>
2022-11-14 03:09:11 +0000

    for read css install style-loader and css-loader & config webpack

--- v-2/src/index.js
+++ v-2/src/index.js
@@ -1,1 +1,7 @@
+import "./assets/css/style.css";
+
 console.log("just for config webpack");
+// const h1 = document.querySelector(".red");
+const h1 = document.getElementsByTagName("h1");
+console.log(h1[0]);
+h1[0].classList.add("red");

e225b108036c423790eb4c85671894bc6d8ff3be
rebwar <rebwar1.azizi@gmail.com>
2022-11-14 01:54:50 +0000

    delet all file and folder inside v-2 folder

--- v-2/src/render/variable.js
+++
@@ -1,7 +0,0 @@
-export const containerAll = document.querySelector(".contanair");
-export const liveSearchName = document.querySelector("#live_search");
-export const buttonName = document.querySelector("#search_name");
-export const buttonEpisode = document.querySelector("#episode-button");
-export const h3 = document.querySelector(".h3");
-export const formSelect = document.querySelector("#form-select");
-export const reset = document.querySelector("#reset");

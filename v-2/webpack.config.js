07dbc8646d6232ad7ffa8de035510d1c8c2fcd22
rebwar <rebwar1.azizi@gmail.com>
2022-11-14 23:51:15 +0000

    we added to run the webpack dev server in development mode__npm install webpack-dev-server --save-dev

--- v-2/webpack.config.js
+++ v-2/webpack.config.js
@@ -53,4 +53,10 @@ module.exports = {
       },
     ],
   },
+  // devServer: {
+  //   contentBase: path.join(__dirname, "build"),
+  //   compress: true,
+  //   port: 3000,
+  //   open: true,
+  // },
 };

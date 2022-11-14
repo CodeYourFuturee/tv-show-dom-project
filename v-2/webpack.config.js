ec272b55f7b84f5b0158c29b2be609e8042a492e
rebwar <rebwar1.azizi@gmail.com>
2022-11-14 09:52:24 +0000

    MiniCssExtractPlugin PLUGINS __  for separate js and CSS from each other and make a new folder main.css inside the src folder

--- v-2/webpack.config.js
+++ v-2/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -6,15 +7,16 @@ module.exports = {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
   },
+  plugins: [new MiniCssExtractPlugin()], // new MiniCssExtractPlugin({filename: "style.css"})
   module: {
     rules: [
       {
         test: /\.css$/i, // css file extension regex test for css files to be processed by css-loader and style-loader
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"], //delete style-loader and add MiniCssExtractPlugin.loader
       },
       {
         test: /\.s[ac]ss$/i, // sass or scss
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"], //delete style-loader and add MiniCssExtractPlugin.loader
       },
       {
         test: /\.(png|jpe?g|gif)$/i, // png, jpg, jpeg, gif
